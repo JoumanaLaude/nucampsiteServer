b27850a4e81f1ba65d1fa092a480df494fb2df51
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-09-07 15:35:30 -0400

    Cookies

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err) // another way to catch errors if you don't plan to chain anything after
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err) // another way to catch errors if you don't plan to chain anything after
 );
 
 var app = express();
@@ -38,28 +38,39 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('133-221-333-123-111'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) { // if user didn't put in credentials
+  if (!req.signedCookies.user) { // signedCookies is from cookie-parser, will automatically parse cookies from the req
+    const authHeader = req.headers.authorization;
+    if (!authHeader) { // if user didn't put in credentials
       const err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
-  }
+    }
 
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); // authorization header, will extract the un and pw
-  const user = auth[0]; // parsing the user and pass from the auth
-  const pass = auth[1];
-  if (user === 'admin' && pass === 'password') {
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); // authorization header, will extract the un and pw
+    const user = auth[0]; // parsing the user and pass from the auth
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', { signed: true });
+      // creating new cookie | 3rd arg is a object that contains configuration values - lets express know to use the secret key from cookie-parser
       return next(); // authorized
-  } else {
+    } else {
       const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
+      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
+    }
+  } else {
+    if (req.signedCookies.user === 'admin') {
+      return next();
+    } else {
+      const err = new Error('You are not authenticated! >:(');
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
@@ -74,12 +85,12 @@ app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
